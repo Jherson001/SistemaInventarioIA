eef0710e62d953191aee251d349404dcd4476d98
Jherson001 <riveramedranoj@gmail.com>
2026-01-07 02:02:42 -0500

    Fix: Variable API_BASE_URL definida y mysql2 instalado

--- frontend/SistemaVentasIA/src/hooks/useApi.js
+++ frontend/SistemaVentasIA/src/hooks/useApi.js
@@ -1,8 +1,7 @@
 // src/hooks/useApi.js
 import useAuth from "./useAuth";
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://sistema-inventario-backend-9im6.onrender.com/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://sistema-inventario-backend-9im6.onrender.com/api';
 export default function useApi() {
   const { token } = useAuth();
 
