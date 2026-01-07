2117a18c1d6572de5b2b20f3a094db4dca51dd8d
Jherson001 <riveramedranoj@gmail.com>
2026-01-07 01:07:12 -0500

    Fix: Conexión final frontend-backend

--- frontend/SistemaVentasIA/src/hooks/useApi.js
+++ frontend/SistemaVentasIA/src/hooks/useApi.js
@@ -1,8 +1,7 @@
 // src/hooks/useApi.js
 import useAuth from "./useAuth";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 export default function useApi() {
   const { token } = useAuth();
 
