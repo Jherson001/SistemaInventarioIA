cc8d1d2b0cf35433f0c42907702ad4fdce7f2580
Jherson001 <riveramedranoj@gmail.com>
2026-01-07 02:06:59 -0500

    Fix: SSL para Aiven y nombre de variable en frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,11 +7,14 @@ dotenv.config();
 
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'inventorydb',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false // <--- AGREGUE ESTO PARA AIVEN
+  },
   multipleStatements: false
 });
 
